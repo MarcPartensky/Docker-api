0565d9d63fdebeea78d0d95e7e454988d3718a11
marc <marc.partensky@gmail.com>
2021-05-02 03:26:55 +0000

    docker commands

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-from typing import Optional
-
-from fastapi import FastAPI
+#!/usr/bin/env python
 
 import docker
 
-client = docker.from_env()
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+client = docker.DockerClient()
 
 app = FastAPI()
 
 
 @app.get("/")
 async def index():
-    return "salut"
+    return "docker-api"
 
 
 @app.get("/containers")
 async def containers():
-    """List all available containers."""
-    return client.containers.list()
+    """List all containers."""
+    return [c.attrs for c in client.containers.list()]
+
+
+@app.get("/containers/ids")
+async def containers_ids():
+    """List all containers ids."""
+    return [c.id for c in client.containers.list()]
 
 
 @app.get("/images")
 async def images():
     """List all available images."""
-    return client.images()
+    return [i.attrs for i in client.images.list()]
 
 
 @app.get("/container/{id}")
 async def container(id: str):
     """Return one container."""
-    return client.containers.get(id)
+    return client.containers.get(id).attrs
 
 
 @app.get("/container/{id}/logs")
@@ -41,13 +51,31 @@ async def container_logs(id: str):
 
 @app.post("/container/{id}/restart")
 async def container_restart(id: str):
-    """Restart one container."""
+    """Restart a container."""
     container = client.containers.get(id)
-    return container.restart()
+    container.restart()
+    return "restarted"
+
+
+@app.post("/container/{id}/kill")
+async def container_kill(id: str):
+    """Kill a container."""
+    container = client.containers.get(id)
+    container.kill()
+    return "killed"
 
 
 @app.post("/container/{id}/stop")
 async def container_stop(id: str):
-    """Stop one container."""
+    """Stop a container."""
     container = client.containers.get(id)
-    return container.stop()
+    container.stop()
+    return "stopped"
+
+
+@app.post("/container/{id}/remove")
+async def container_stop(id: str):
+    """Stop a container."""
+    container = client.containers.get(id)
+    container.remove()
+    return "removed"
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-alias run="uvicorn main:app --reload"
+alias run="uvicorn main:app --host 0.0.0.0 --port 8000"
 
 install() {
 	python -m pip install pipenv
 	pipenv install
 }
+
+run
