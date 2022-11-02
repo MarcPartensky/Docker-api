664356cbfe3b0813d9f6f8d96d7f90c27c3037e7
Marc Partensky <marc.partensky@gmail.com>
2022-11-02 23:42:26 +0100

    fuckcors

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import docker
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 client = docker.DockerClient()
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 
 
 @app.get("/")
