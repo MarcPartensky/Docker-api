c8ad1bdb0924a7c4cc35759c9b588c5ea7f5293c
Marc Partensky <marc.partensky@gmail.com>
2022-11-03 22:11:41 +0100

    fast api

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import docker
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from fastapi.middleware.cors import CORSMiddleware
 
 client = docker.from_env()
@@ -22,6 +23,25 @@ app.add_middleware(
     allow_methods=[""],
     allow_headers=[""],
 )
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
 
 
 @app.get("/")
